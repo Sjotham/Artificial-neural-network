8a6e13fe7999d7745ee6b58349bd435ff5192ec8
Sanele Dlamini <sjothamdlamini@gmail.com>
2024-04-11 16:37:48 +0200

    This is my project initial state.

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a machine learning assignment 1: Articial neural network
+Its an ANN for image classification 
+We using MNIST dataset 
---
+++ classifier.py
@@ -0,0 +1,66 @@
+import torch
+import torchvision
+from torchvision import datasets, transforms
+from torch import nn, optim
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,))])
+
+# Download and load the training data
+trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+# Download and load the test data
+testset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+
+class DigitClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden = nn.Linear(784, 128) # Input layer to hidden layer
+        self.output = nn.Linear(128, 10) # Hidden layer to output layer
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.hidden(x)
+        x = self.sigmoid(x)
+        x = self.output(x)
+        x = self.softmax(x)
+        return x
+
+
+model = DigitClassifier()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.03)
+
+
+epochs = 15
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        # Flatten MNIST images into a 784 long vector
+        images = images.view(images.shape[0], -1)
+
+        optimizer.zero_grad()
+
+        output = model(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+    else:
+        print(f"Training loss: {running_loss / len(trainloader)}")
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for images, labels in testloader:
+        images = images.view(images.shape[0], -1)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images: {100 * correct / total}%')
